18db316c79c79127cc821fd60d2dc6c637ad3959
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2024-02-15 11:33:35 +0100

    sidebar-correction

--- src/layout/main-layout/sidebar/index.tsx
+++ src/layout/main-layout/sidebar/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Menu from "./menu";
+import classNames from "classnames";
 
 interface SidebarProps {
   open: any;
@@ -8,9 +9,11 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ open }) => {
   return (
     <div
-      className={`w-60 p-4 ${
-        open ? "block" : "hidden"
-      } md:block fixed left-0 bottom-0 h-full md:static md:h-auto`}
+      className={classNames("w-60", "p-4",
+        { block: open, hidden: !open },
+        "md:block","fixed", "left-0", "bottom-0", "h-full",
+        { static: !open, "md:h-auto": open }
+      )}
     >
       <div className="w-full">
         <Menu />
