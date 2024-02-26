b874e702a1db0b8a939ea983b557cca5dd662151
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2024-02-26 14:27:40 +0100

    blacklist-report-details

---
+++ src/views/blacklist/blacklist-details/previous-reports.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PreviousReport = () => {
+  return (
+    <section className="w-full px-10">
+           PreviousReport
+    </section>
+  )
+}
+
+export default PreviousReport
