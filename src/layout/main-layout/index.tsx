587fb11be2b05ba970c4c7f61cec04ca8ef0ba85
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2024-06-22 23:57:34 +0100

    details

--- src/layout/main-layout/index.tsx
+++ src/layout/main-layout/index.tsx
@@ -36,16 +36,17 @@ const DashboardLayout = () => {
   }, [sidebarBackgroundCheck]);
 
   return (
-    <div className="max-w-1500 mx-auto flex flex-col md:flex-row pt-5">
+    <div className="max-w-1500 mx-auto flex flex-col md:flex-row">
       {showSidebar && (
         <aside
           className={classNames(
-            "bg-gray-100  transition-all duration-300 h-[140vh] border-r border-gray-200",
+            "transition-all duration-300 h-full overflow-y-auto overflow-x-hidden fixed border-r border-gray-200 ",
             {
               "w-64": sidebarOpen,
               "w-0": !sidebarOpen,
             }
           )}
+          style={{ height: "100vh" }}
         >
           <div className="p-4">
             <Link to='/'>
@@ -61,10 +62,11 @@ const DashboardLayout = () => {
         className={classNames(
           "py-2 md:py-5 flex-grow transition-all duration-300",
           {
-            "pl-12": sidebarOpen,
-            "pl-2 md:pl-3.5": !sidebarOpen,
+            "ml-70": sidebarOpen,
+            " ml-0 pl-2 md:pl-3.5": !sidebarOpen,
           }
         )}
+        style={{ marginLeft: sidebarOpen ? "19rem" : "0" }}
       >
         <Outlet />
       </main>
